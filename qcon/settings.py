74942ce74bcfa61baedc95c77ec2e4f9e4333f7a
Viresh Soedhwa <vsoedhwa@bcit.ca>
2026-02-19 10:49:27 -0800

    feat: enable token authentication for REST framework
    - Uncommented TokenAuthentication in DEFAULT_AUTHENTICATION_CLASSES
    - Enables token-based authentication for API endpoints

--- qcon/settings.py
+++ qcon/settings.py
@@ -278,7 +278,7 @@ REST_FRAMEWORK = {
         # 'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         # 'rest_framework.authentication.BasicAuthentication',
         # 'api_v2.authentication.ExampleAuthentication',
     ]
